f7a47aaf281a15b209e073dd3e3a02756703d3fd
NotClassified <79024057+NotClassified@users.noreply.github.com>
2023-06-14 09:14:42 -0700

    Member vars for task data

--- Assets/Scripts/Task List/TaskObjectData.cs
+++ Assets/Scripts/Task List/TaskObjectData.cs
@@ -7,6 +7,28 @@ public class TaskObjectData
 {
     public string name;
     public bool completed;
-    public string groupName;
-    public bool isGroup;
+    public int id;
+
+    public int parent_ID;
+    public int child_ID;
+    public int nextSibling_ID;
+    public int prevSibling_ID;
+
+    public TaskObjectData(string name, int id, int parent_ID)
+    {
+        this.name = name;
+        this.id = id;
+        this.parent_ID = parent_ID;
+    }
+
+    public TaskObjectData(TaskObjectData clone)
+    {
+        this.name = clone.name;
+        this.completed = clone.completed;
+        this.id = clone.id;
+        this.parent_ID = clone.parent_ID;
+        this.child_ID = clone.child_ID;
+        this.nextSibling_ID = clone.nextSibling_ID;
+        this.prevSibling_ID = clone.prevSibling_ID;
+    }
 }
