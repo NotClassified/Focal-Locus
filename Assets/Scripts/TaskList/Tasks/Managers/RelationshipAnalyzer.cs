570675b48ac75602ce5c1cdb82325935bd856d0b
NotClassified <79024057+NotClassified@users.noreply.github.com>
2023-07-08 19:29:38 -0700

    StartDev - RelationshipAnalyzer

---
+++ Assets/Scripts/TaskList/Tasks/Managers/RelationshipAnalyzer.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RelationshipAnalyzer : MonoBehaviour
+{
+    [SerializeField] GameObject prefab;
+    GameObject toggleParent;
+    GameObject listParent;
+    TaskListDataManager dataManager;
+
+    private void Awake()
+    {
+        dataManager = GetComponent<TaskListDataManager>();
+        toggleParent = Instantiate(prefab, transform);
+        listParent = transform.Find("Scroll List").gameObject;
+    }
+
+    public void ShowRelationships()
+    {
+        listParent.SetActive(false);
+        toggleParent.SetActive(true);
+
+        var list = dataManager.currentData.lists[dataManager.currentData.dayIndex];
+
+    }
+}
